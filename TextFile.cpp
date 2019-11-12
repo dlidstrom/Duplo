60c847dc135faac503cd85bfac8626b5f337223c
Daniel Lidstrom <dlidstrom@gmail.com>
2019-11-12 16:44:39 +0100

    Cleaned up license

--- TextFile.cpp
+++ TextFile.cpp
@@ -1,26 +1,9 @@
-/**
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
+#include "TextFile.h"
+#include "StringUtil.h"
 
 #include <fstream>
 #include <iostream>
 #include <sstream>
-
-#include "TextFile.h"
-
-#include "StringUtil.h"
 
 /**
  * Creates a new text file. The file is accessed relative to current directory.
