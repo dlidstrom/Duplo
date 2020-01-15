eefae740a21c4f3df3e39ded3cb9226c312c9b81
Daniel Lidstrom <dlidstrom@gmail.com>
2020-01-15 11:23:47 +0100

    Continuous build with CMake

--- compile.sh
+++ compile.sh
@@ -4,8 +4,19 @@ p() {
     printf "$(tput setaf 1)%s$(tput sgr0) | $(tput bold)$1$(tput sgr0)\n" "$now";
 }
 
+err() {
+    echo "Error: $*" >> /dev/stderr
+}
+
+if [ ! -d "build" ]; then
+    err "You need to initialize first."
+    err "Create a \"build\" folder and issue \"cmake ..\" in there."
+    exit
+fi
+
 p "compiling..." &&
+pushd build
 make &&
 
 p "OK!" &&
-p "waiting for new changes...";
+popd;
