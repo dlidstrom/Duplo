04a3f4fd22bc1e3bf99ac568037393e5933fae0b
Christos Gkantidis <cgkantid@proton.me>
2025-03-21 18:58:04 +0100

    Add the option to write the results to a JSON file
    JSON has wider support than XML across programming languages, so I believe
    having the option to write the findings in a JSON format make Duplo more
    versatile, since its output can be consumed by many more tools.
    
    I chose to support JSON by adding a dependancy to the nlohmann/json header-only
    library, which handles escaping of the lines correctly.
    
    This has the drawback that we keep all the findings in memory (in a `json`
    object) instead of writing them directly to the output file.
    
    If this is considered a deal-breaker, we can re-implement all the escaping of
    the lines ourselves, as is done for XML, and write directly to the output file.

--- src/Main.cpp
+++ src/Main.cpp
@@ -29,6 +29,7 @@ namespace {
             auto minChars = ap.getInt("-mc", MIN_CHARS);
             bool ignorePrepStuff = ap.is("-ip");
             bool outputXml = ap.is("-xml");
+            bool outputJSON = ap.is("-json");
             bool ignoreSameFilename = ap.is("-d");
             std::string listFilename(argv[argc - 2]);
             std::string outputFilename(argv[argc - 1]);
@@ -39,6 +40,7 @@ namespace {
                 blockPercentThreshold,
                 numberOfFiles,
                 outputXml,
+                outputJSON,
                 ignoreSameFilename,
                 listFilename,
                 outputFilename);
@@ -64,6 +66,7 @@ namespace {
             std::cout << "       -ip              ignore preprocessor directives\n";
             std::cout << "       -d               ignore file pairs with same name\n";
             std::cout << "       -xml             output file in XML\n";
+            std::cout << "       -json            output file in JSON format\n";
             std::cout << "       INPUT_FILELIST   input filelist (specify '-' to read from stdin)\n";
             std::cout << "       OUTPUT_FILE      output file (specify '-' to output to stdout)\n";
 
